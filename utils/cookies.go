7de38e5498db5dc3f344cf72da44433c82c9d76c
tristan-mihai.radulescu <tristan-mihai.radulescu@etu.umontpellier.fr>
2024-11-03 18:41:12 +0100

    fix(GIST-70): sign out correctly

--- utils/cookies.go
+++ utils/cookies.go
@@ -1,12 +1,17 @@
 package utils
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func Cookie(key string, value string) *fiber.Cookie {
 	cookie := new(fiber.Cookie)
 	cookie.Name = key
 	cookie.HTTPOnly = true
 	cookie.Value = value
+	cookie.Expires = time.Now().Add(time.Hour * 24 * 30 * 12) // 1 year
 
 	if Get("ENV") == "development" {
 		cookie.Secure = false
@@ -14,5 +19,21 @@ func Cookie(key string, value string) *fiber.Cookie {
 		cookie.Domain = ".gists.app" // hardcoded
 		cookie.Secure = true
 	}
+	return cookie
+}
+
+func ClearCookie(key string) *fiber.Cookie {
+	cookie := new(fiber.Cookie)
+	cookie.Name = key
+	cookie.HTTPOnly = true
+	cookie.Value = ""
+	cookie.Expires = time.Now().Add(-time.Hour)
+	cookie.Secure = true
+	if Get("ENV") == "development" {
+		cookie.Secure = false
+	} else {
+		cookie.Domain = ".gists.app" // hardcoded
+	}
+
 	return cookie
 }
